d8867bf21c38972d227a2f87ababa45e5b1e89b9
洛丹 <weichun.swc@alibaba-inc.com>
2019-02-14 15:11:22 +0800

    pages

--- lib/render.d.ts
+++
@@ -1,4 +0,0 @@
-import { RenderOptions, ITemplateResult } from './type';
-import { NodePart } from './part';
-export declare function shadowRender(result: ITemplateResult, container: Element | DocumentFragment, options?: Partial<RenderOptions>): NodePart;
-export declare function render(): void;
--- src/util/constant.ts
+++
@@ -1,3 +0,0 @@
-
-
-export const DEFAULT_SLOT_NAME = '@slot@'
