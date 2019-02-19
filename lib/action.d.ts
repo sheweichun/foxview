8bf12ec60e7505318e114b17a15fa0f646fdb584
洛丹 <weichun.swc@alibaba-inc.com>
2019-02-19 23:29:14 +0800

    fix remove lib dist

--- lib/action.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const SET_ATTRIBUTE_VALUE = 1;
-export declare const SET_ATTRIBUTE = 2;
-export declare const SET_CONTENT = 3;
-export declare function parseAction(s: string): void;
--- lib/component-registry.d.ts
+++
@@ -1,3 +0,0 @@
-import { IComponentConstructor } from './type';
-export declare function defineComponent(name: string, ComponentProto: IComponentConstructor): void;
-export declare function getCom(name: string): IComponentConstructor;
